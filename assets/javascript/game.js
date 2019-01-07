1d7f155d85e0bb66eace7681cb1b1ae15f849307
John Evans <evanjo03@luther.edu>
2019-01-07 16:14:51 -0600

    still goin

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     //audio files
-    var audio = new Audio("assets/sounds/background.mp3");
+    var audio = new Audio("assets/Sounds/background.mp3");
     Audio.loop = true; // loop background music
-    var winAudio = new Audio("assets/sounds/win.mp3");
-    var loseAudio = new Audio("assets/sounds/lose.mp3");
+    var winAudio = new Audio("assets/Sounds/win.mp3");
+    var loseAudio = new Audio("assets/Sounds/lose.mp3");
 
     //win/loss variables
     var wins = 0; 
