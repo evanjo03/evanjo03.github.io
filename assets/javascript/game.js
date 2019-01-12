1b8cc09a1b7ff52bcff8f10848e8004d3d80ab80
John Evans <evanjo03@luther.edu>
2019-01-12 11:57:16 -0600

    changed click events

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,15 +6,24 @@ $(document).ready(function () {
     var loseAudio = new Audio("assets/audio/lose.mp3");
 
     //win/loss variables
-    var wins = 0; 
+    var wins = 0;
     var losses = 0;
 
     var myScore = 0; //score, targetscore, and buttonvalues reset upon win/loss
     var ranNum = (Math.floor(Math.random() * 101) + 19); //number needed to win
-    var buttonOneVal = (Math.floor(Math.random() * 12) + 1);// value assigned to button one
-    var buttonTwoVal = (Math.floor(Math.random() * 12) + 1);// value assigned to button two
-    var buttonThreeVal = (Math.floor(Math.random() * 12) + 1);// value assigned to button three
-    var buttonFourVal = (Math.floor(Math.random() * 12) + 1);// value assigned to button four
+    var buttonValues = [];
+    for (var i = 0; i < 4; i++) {
+        buttonValues.push(Math.round(Math.random() * 12))
+    }
+    $('#button1').attr('value', buttonValues[0]);
+
+    $('#button2').attr('value', buttonValues[1]);
+
+    $('#button3').attr('value', buttonValues[2]);
+
+    $('#button4').attr('value', buttonValues[3]);
+    console.log(buttonValues);
+
     $("#goal").text("Target Score: " + ranNum);
 
     //background music toggle on
@@ -29,82 +38,26 @@ $(document).ready(function () {
     function reset() {
         myScore = 0; //variables are reset
         ranNum = (Math.floor(Math.random() * 101) + 19); //number needed to win
-        buttonOneVal = (Math.floor(Math.random() * 12) + 1);// value assigned to button one
-        buttonTwoVal = (Math.floor(Math.random() * 12) + 1);// value assigned to button two
-        buttonThreeVal = (Math.floor(Math.random() * 12) + 1);// value assigned to button three
-        buttonFourVal = (Math.floor(Math.random() * 12) + 1);// value assigned to button four
+        var buttonValues = [];
+        for (var i = 0; i < 4; i++) {
+            buttonValues.push(Math.round(Math.random() * 12))
+        }
+        $('#button1').attr('value', buttonValues[0]);
+
+        $('#button2').attr('value', buttonValues[1]);
+
+        $('#button3').attr('value', buttonValues[2]);
+
+        $('#button4').attr('value', buttonValues[3]);
         $("#goal").text("New Target Score: " + ranNum);
     }
     // on a click event for each crystal, add value of the crystal to the player's score (use this)
 
-    //ALL BUTTON CLICK EVENTS BELOW
-    
-    //button one click
-    $("#button1").on("click", function () { 
-        $("#alert").empty();
-        myScore += buttonOneVal;
-        if (myScore == ranNum) {
-            $("#alert").text("You win! Click another crystal to play again!")
-            wins += 1;
-            winAudio.play();
-            reset();
-        }
-        else if (myScore > ranNum) {
-            $("#alert").text("You lose! Click another crystal to play again!")
-            losses += 1;
-            loseAudio.play();
-            reset();
-        }
-        $("#myScoreElement").text("Score: " + myScore);
-        $("#wins").text("Wins: " + wins);
-        $("#losses").text("Losses: " + losses);
-    });
 
-    //button two click
-    $("#button2").on("click", function () {
+    //button click
+    $(".crystals").on("click", function () {
         $("#alert").empty();
-        myScore += buttonTwoVal;
-        if (myScore == ranNum) {
-            $("#alert").text("You win! Click another crystal to play again!")
-            wins += 1;
-            winAudio.play();
-            reset();
-        }
-        else if (myScore > ranNum) {
-            $("#alert").text("You lose! Click another crystal to play again!")
-            losses += 1;
-            loseAudio.play();
-            reset();
-        }
-        $("#myScoreElement").text("Score: " + myScore);
-        $("#wins").text("Wins: " + wins);
-        $("#losses").text("Losses: " + losses);
-    });
-
-    //button three click
-    $("#button3").on("click", function () {
-        $("#alert").empty();
-        myScore += buttonThreeVal;
-        if (myScore == ranNum) {
-            $("#alert").text("You win! Click another crystal to play again!")
-            wins += 1;
-            winAudio.play();
-            reset();
-        }
-        else if (myScore > ranNum) {
-            $("#alert").text("You lose! Click another crystal to play again!")
-            losses += 1;
-            loseAudio.play();
-            reset();
-        }
-        $("#myScoreElement").text("Score: " + myScore);
-        $("#wins").text("Wins: " + wins);
-        $("#losses").text("Losses: " + losses);
-    });
-    //button four click
-    $("#button4").on("click", function () {
-        $("#alert").empty();
-        myScore += buttonFourVal;
+        myScore += parseInt($(this).attr('value'));
         if (myScore == ranNum) {
             $("#alert").text("You win! Click another crystal to play again!")
             wins += 1;
@@ -122,3 +75,4 @@ $(document).ready(function () {
         $("#losses").text("Losses: " + losses);
     });
 });
+  
